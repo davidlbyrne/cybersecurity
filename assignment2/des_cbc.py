afbb9e0cbe25821122d0e353caa5c26e7e9d477c
David Byrne <davidbyrne@David-Byrnes-MacBook-Pro.local>
2018-10-27 17:59:22 -0700

    left off not workoing

--- assignment2/des_cbc.py
+++ assignment2/des_cbc.py
@@ -1,30 +1,26 @@
+#!/usr/local/bin/python3
 # Assignment2 - Assignment 2, due November 7, 2018: Problem 6.1
 # in the lecture notes. You may import the following packages from
 # Python libraries:
 
 import sys
+import binascii
 from Crypto.Cipher import DES
 from Crypto import Random
 
 
-#setup and validate input 
-# Set up key , plain text , cipher text , and IV
-key = sys.argv[1]
-file = open(sys.argv[2] , ” r ” )
-plaintext=file.read()
-file.close()
-ciphertext = ””
-iv = Random.new().read(DES.blocksize)
+    
 
 def checkpad(plaintext):
-    lenght= len(plaintext)
-    rem = length%8
-    # Pad indicates how many characters we have to add to the end of the file
-    pad = 8 - rem
-    # the first meaningless number in binary is (10000000) which is equal to 128
-    z= 128
-    for i in range (pad):
-        plaintext= plaintext + chr (z + pad)
+    length= len(plaintext)
+    if ((length % 8) != 0):
+        rem = length%8
+        # Pad indicates how many characters we have to add to the end of the file
+        pad = 8 - rem
+        # the first meaningless number in binary is (10000000) which is equal to 128
+        z= 128
+        for i in range (pad):
+            plaintext= plaintext + chr (z + pad)
     return (plaintext)
 
 
@@ -32,62 +28,62 @@ def checkpad(plaintext):
 # xor function
 def xor(lft, rht):
     # set up 8 char arrays with none elements
-    lft_ba = [None] * 8
-    rht_ba = [None] * 8
-
+    lft_ba = [None,None,None,None,None,None,None,None]
+    rht_ba = [None]*8
+    print(lft_ba)
+    print(lft, rht)
     # step through each letter and conv to integer and store
-    for i in range(0, 8):
+    for i in range(0,8):
+        print(i)
         lft_ba[i] = ord(lft[i])
         rht_ba[i] = ord(rht[i])
-
     # finally the result of xor
     xor_res = [None] * 8
     for i in range(0, 8):
         xor_res[i] = lft_ba[i] ^ rht_ba[i]
-
     # convert the bits into char again
-    char_xor = ''
-    for k in xor_res:
-        char_xor.join(chr(k))
-
-    return char_xor
+    ret=""
+    for i in xor_res:
+        ret = ret+chr(i)
+    print(ret)
+    return ret
 
 #Encrypt function
-des_cbc_encrypt(plaintext,key) :
+def des_cbc_encrypt(plaintext,key,iv) :
+    ciphertext="test"
     #des first round 
-    obj = DES.new(key,DES.MODE_CBC) 
+    obj = DES.new(key,DES.MODE_ECB) 
     plaintext = checkpad(plaintext)
     pte = plaintext[:8]
     plaintext = plaintext[8:]
-    pte = xor(pte,iv)
-    previous = obj.encrypt(pte) 
-    ciphertext = ciphertext + obj.encrypt(pte)
+    print("pte:",pte)
+    pte = xor(iv,pte)
+    previous = str(obj.encrypt(pte)) 
+    ciphertext = ciphertext + str(obj.encrypt(pte))
     # begin remaining rounds
-    for i in range (0,(len(plaintext)/8)) : 
+    for i in range (0,int((len(plaintext)/8))) : 
         pte = checkpad(plaintext[:8])
         plaintext = plaintext[8:]
-        pte = xor(pte,previous)
+        print("previous ",previous)
+        pte = xor(previous,pte)
         previous = obj.encrypt(pte)
-        ciphertext=ciphertext+obj.encrypt(pte)
+        ciphertext=ciphertext+str(obj.encrypt(pte))
     return ciphertext
 #Decrypt Function 
 #des first round 
 #remaining rounds 
 
 
+if __name__ == '__main__':
+    key = sys.argv[1]
+    print("Encrypting :",sys.argv[2])
+    file = open(sys.argv[2],'r')
+    plaintext=file.read()
+    file.close()
+    iv = '00000000'
+    ciphertext=des_cbc_encrypt(plaintext,key,iv)
+    print(ciphertext)
 #print 
-=======
-        ciphertext = ciphertext + obj.encrypt(pte) # add on to c i p h e r t e x t
-
-# remaining rounds
->>>>>>> feature/decrypt_dec
-
-#Decrypt Function
-#des first round
-#remaining rounds
-
-
-#print
 
 
 '''
